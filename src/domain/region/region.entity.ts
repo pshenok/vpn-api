8a9cbd41ba76d986c56fbf834aa4014700a7b587
Kostya P <k.pshenychnyy@gmail.com>
2022-06-17 22:29:13 -0700

    feat(server): domain.

--- src/domain/region/region.entity.ts
+++ src/domain/region/region.entity.ts
@@ -1,17 +1,18 @@
 import { v4 as uuid } from 'uuid';
+import { Server } from '../server/server.entity';
 
 export class Region {
   public id: string;
   public name: string;
   public icon: string;
   public iso: string;
-  public servers: string;
+  public servers: Server[];
 
   constructor(init: Partial<Region>) {
     this.id = init.id || uuid();
     this.name = init.name!;
     this.icon = init.icon!;
     this.iso = init.iso!;
-    this.servers = init.servers!;
+    this.servers = init.servers || [];
   }
 }
---
+++ src/domain/server/server.entity.ts
@@ -0,0 +1,17 @@
+import { v4 as uuid } from 'uuid';
+
+export class Server {
+  public id: string;
+  public city: string;
+  public ip: string;
+  public key: string;
+  public regionId: string;
+
+  constructor(init: Partial<Server>) {
+    this.id = init.id || uuid();
+    this.city = init.city!;
+    this.ip = init.ip!;
+    this.key = init.key!;
+    this.regionId = init.regionId!;
+  }
+}
